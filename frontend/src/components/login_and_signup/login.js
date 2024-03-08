49ef61109b33c2634d331fd4efe71be62eebbabc
Kashyap Shukla <kshukl03@nyit.edu>
2024-03-07 19:10:21 -0500

    new commit

--- frontend/src/components/login_and_signup/login.js
+++ frontend/src/components/login_and_signup/login.js
@@ -1,19 +1,136 @@
 import React, { useState } from 'react';
 
-import { useNavigate } from 'react-router-dom';
+
+
+import { useNavigate} from 'react-router-dom';
+import TextField from '@mui/material/TextField';
+import Box from '@mui/material/Box';
 
 
 export function LoginStudent(){
 
+
+  const [state, setState] = useState({
+    email: '',
+    password: '',
+    isLoggedIn: false,
+    userId: '0',
+  });
+  // const nv = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const { email, password } = state;
+    console.log(email,password)
+
+    fetch('http://localhost:5003/login-user', {
+      method: 'POST',
+      crossDomain: true,
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+        'Access-Control-Allow-Origin': '*',
+      },
+      body: JSON.stringify({
+        email,
+        password,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.status === 'ok') {
+
+          setState({ ...state, isLoggedIn: true, userId: data.data._id });
+          // nv(`/users_edit_or_new/${data.data._id}`);
+        }
+      });
+  };
+
+
+
+
+   
+
     return(
        
-       <>
-       <div>
-        <h1>
-            This is Login page.
+        
+      <div className='container'>
+      <div className='col1'>
+        
+      
+        
+       
+        <form onSubmit={handleSubmit} className='form'>
+      
+        <h3 className='login'>Log In</h3>
+        <h4 className='line2'>Please fill your information below </h4>
+      
 
-        </h1>
-       </div>
-     </>
+        <Box
+      
+     
+    >
+
+        <TextField
+         
+          
+          id="outlined-helperText"
+          label="Email"
+          defaultValue=""
+         
+          onChange={(e) => setState({ ...state, email: e.target.value })}
+        />
+
+        <TextField
+          id="outlined-password-input"
+          label="Password"
+          type="password"
+          autoComplete="current-password"
+          onChange={(e) => setState({ ...state, password: e.target.value })}
+
+        />
+
+          <button type='submit' className='b11'>
+            Next &gt;
+          </button>
+
+        </Box>
+          
+       
+
+
+       
+
+        <div className='b22'>
+          
+        </div>
+       
+        </form>
+      </div>
+
+       
+        
+      
+    </div>
+
     );
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
